0b54da9f451679401448297b62572085f72882d8
Atharva6699 <atharvagawande0609@gmail.com>
2025-07-27 20:04:44 +0530

    .

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;  // You can change this value
+        int b = 20;  // You can change this value
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+    }
+}
